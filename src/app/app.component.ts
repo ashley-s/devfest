7d9bef700255a68a6938f931c5fee41c02c37ee5
Ashley Shookhye <ashley.shookhye@mra.mu>
2019-10-03 10:11:48 +0400

    first import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup, NgForm, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-forms2';
+export class AppComponent implements OnInit {
+  //name: FormControl;
+  nameValue: string;
+  mainForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    
+  }
+
+  ngOnInit() {
+    //this.name = new FormControl("", [Validators.required, Validators.minLength(5)]);
+    //this.name.setValidators();
+    // this.mainForm = new FormGroup({
+    //   name: new FormControl("", [Validators.required, Validators.minLength(5)])
+    // })
+    this.mainForm = this.fb.group({
+      fname: ["", [Validators.required, Validators.minLength(5)]],
+      lname: ["", [Validators.required]]
+    })
+    //this.onChanges();
+  }
+
+  get fname() {
+    return this.mainForm.get('fname');
+  }
+
+  get lname() {
+    return this.mainForm.get('lname');
+  }
+
+  submitForm(form: NgForm) {
+    console.log('-----------');
+    console.log(form.status);
+    console.log('-----------');
+    console.log('-----------');
+    console.log(form.value);
+    console.log('-----------');
+  }
 }
