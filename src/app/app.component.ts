2119bab98107973ffc166872984b10ab106ef0b9
Ashley Shookhye <ashley.shookhye@mra.mu>
2019-10-03 10:10:23 +0400

    first import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-forms1';
+export class AppComponent implements OnInit {
+  name: FormControl;
+  nameValue: string;
+  mainForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    //this.name = new FormControl("", [Validators.required, Validators.minLength(5)]);
+    this.name = this.fb.control("", [Validators.required, Validators.minLength(5)])
+    //this.name.setValidators();
+    this.onChanges();
+  }
+
+  onChanges() {
+    const nameCtrl$ = this.name.valueChanges;
+
+    nameCtrl$.subscribe((data) => {
+      this.nameValue = data;
+    })
+  }
 }
